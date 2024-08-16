27242be2c4f1322cbe077fbf78fd121535bf19e4
whoispratik <pratikthapa567@gmail.com>
2024-08-16 18:11:49 +0545

    a slight change to the hero section

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,10 +1,5 @@
 <template>
-  <div
-    class="hero min-h-screen"
-    style="
-      background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp);
-    "
-  >
+  <div class="hero min-h-screen" style="background-image: url(/assets/img/backg4.jpg)">
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-neutral-content text-center">
       <div class="max-w-md">
